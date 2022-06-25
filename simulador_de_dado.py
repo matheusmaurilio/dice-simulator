cf0b6d9fdd26b4b373b9b7f4558505e049b1864e
Matheus Maurilio <mathmaurilio@gmail.com>
2022-06-25 15:35:30 -0300

    Versão 1.01

--- simulador_de_dado.py
+++ simulador_de_dado.py
@@ -7,17 +7,26 @@ class Simulador_de_dado:
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
-        self.mensagem = 'Você gostaria de gerar um novo valor para o dado? "s ou n" '
-
+        self.mensagem = 'Você gostaria de gerar um novo valor para o dado? "sim ou não" '
+        #layout
+        self.layout = [
+            [PSG.Text('Jogar o dado? ')],
+            [PSG.Button('sim'), PSG.Button('não')]
+        ]
+        
     def Iniciar(self): 
-        msg = input(self.mensagem)
+        #criar uma janela
+        self.janela = PSG.Window('Simulador de Dado',self.layout)
+        #ler os valores da tela
+        self.eventos, self.valores = self.janela.Read()
+        #fazer alguma coisa com esses valores
         try:
-            if msg == "s":
+            if self.eventos == 'sim':
                 self.GerarValorDado()
-            elif msg == "n":
+            elif self.eventos == "não":
                 print('Obrigado por jogar! ')  
             else:
-                print('Favor digitar s ou n !')
+                print('Favor digitar sim ou não!')
         except:
             print('Ocorreu um erro com a sua resposta!')
 
